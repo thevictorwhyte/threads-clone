0e0d496dd0d360f37a5510cb7b5e0b93edfaed2f
Victor Whyte <victordavidwhyte@gmail.com>
2023-08-15 18:05:05 +0100

    feat: setup mongodb and logic to upsert a user

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -1,5 +1,7 @@
 'use client';
 import {useForm} from 'react-hook-form';
+import {usePathname, useRouter} from 'next/navigation';
+
 import {Button} from '@/components/ui/button';
 import {
   Form,
@@ -17,6 +19,7 @@ import {ChangeEvent, useState} from 'react';
 import {Textarea} from '../ui/textarea';
 import {isBase64Image} from '@/lib/utils';
 import {useUploadThing} from '@/lib/uploadthing';
+import {updateUser} from '@/lib/actions/user.actions';
 
 interface Props {
   user: {
@@ -33,6 +36,8 @@ interface Props {
 const AccountProfile = ({user, btnTitle}: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const {startUpload} = useUploadThing('media');
+  const router = useRouter();
+  const pathname = usePathname();
 
   const form = useForm<z.infer<typeof UserValidation>>({
     resolver: zodResolver(UserValidation),
@@ -81,7 +86,20 @@ const AccountProfile = ({user, btnTitle}: Props) => {
       }
     }
 
-    // TODO: Update user profile
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+    if (pathname === '/profile/edit') {
+      router.back();
+    } else {
+      router.push('/');
+    }
   };
 
   return (
