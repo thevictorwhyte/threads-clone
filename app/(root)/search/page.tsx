1dfdb93566731618d4fd8d7664aac615cef67224
Victor Whyte <victordavidwhyte@gmail.com>
2023-08-17 11:26:33 +0100

    feat: implement search users display feature and the fetchUsers actino

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,7 +1,48 @@
+import UserCard from '@/components/cards/UserCard';
+import {fetchUser, fetchUsers} from '@/lib/actions/user.actions';
+import {currentUser} from '@clerk/nextjs';
+import {redirect} from 'next/navigation';
+
 const Page = async () => {
+  const user = await currentUser();
+
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+
+  if (!userInfo?.onboarded) redirect('/onboarding');
+
+  // Fetch users
+  const result = await fetchUsers({
+    userId: user.id,
+    searchString: '',
+    pageNumber: 1,
+    pageSize: 25,
+  });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
+      {/* Search bar */}
+      <div className="mt-14 flex flex-col gap-9">
+        {result.users.length === 0 ? (
+          <p className="no-result">No usdrs</p>
+        ) : (
+          <>
+            {result.users.map(person => {
+              return (
+                <UserCard
+                  key={person.id}
+                  id={person.id}
+                  name={person.name}
+                  username={person.username}
+                  imgUrl={person.image}
+                  personType="User "
+                />
+              );
+            })}
+          </>
+        )}
+      </div>
     </section>
   );
 };
