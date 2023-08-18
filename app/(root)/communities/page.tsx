fe0a3072ba35f0c2377b97c983863f8261fb61be
Victor Whyte <victordavidwhyte@gmail.com>
2023-08-18 09:26:17 +0100

    feat: implement search on communities and creators

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,11 +1,16 @@
 import CommunityCard from '@/components/cards/CommunityCard';
 import UserCard from '@/components/cards/UserCard';
+import Searchbar from '@/components/shared/Searchbar';
 import {fetchCommunities} from '@/lib/actions/community.actions';
 import {fetchUser, fetchUsers} from '@/lib/actions/user.actions';
 import {currentUser} from '@clerk/nextjs';
 import {redirect} from 'next/navigation';
 
-const Page = async () => {
+const Page = async ({
+  searchParams,
+}: {
+  searchParams: {[key: string]: string | undefined};
+}) => {
   const user = await currentUser();
 
   if (!user) return null;
@@ -16,17 +21,20 @@ const Page = async () => {
 
   // Fetch Communities
   const result = await fetchCommunities({
-    searchString: '',
-    pageNumber: 1,
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
     pageSize: 25,
   });
   return (
     <section>
       <h1 className="head-text mb-10">Search</h1>
       {/* Search bar */}
-      <div className="mt-14 flex flex-col gap-9">
+      <div className="mt-5">
+        <Searchbar routeType="communities" />
+      </div>
+      <div className="mt-9 flex flex-wrap gap-4">
         {result.communities.length === 0 ? (
-          <p className="no-result">No usdrs</p>
+          <p className="no-result">No communities</p>
         ) : (
           <>
             {result.communities.map(community => {
