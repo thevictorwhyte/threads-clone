1aad298d4ed67c508731596a3563a0bfd0f7c2d3
Victor Whyte <victordavidwhyte@gmail.com>
2023-08-18 11:30:08 +0100

    feat: show replies icon below

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,6 +1,7 @@
 import {formatDateString} from '@/lib/utils';
 import Image from 'next/image';
 import Link from 'next/link';
+import DeleteThread from '../forms/DeleteThread';
 
 interface Props {
   id: string;
@@ -42,7 +43,7 @@ const ThreadCard = ({
       className={`flex w-full flex-col rounded-xl  ${
         isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'
       }`}>
-      <div className="flex flex-col items-start justify-between">
+      <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
           <div className="flex flex-col items-center">
             <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
@@ -111,26 +112,55 @@ const ThreadCard = ({
           </div>
         </div>
 
-        {/* TODO: Delete thread */}
-        {/* TODO: Show comment logos */}
-        {!isComment && community && (
-          <Link
-            href={`/communities/${community.id}`}
-            className="mt-5 flex items-center">
-            <p className="text-subtle-medium text-gray-1">
-              {formatDateString(createdAt)} - {community.name} Community
-            </p>
-
-            <Image
-              src={community.image}
-              alt={community.name}
-              width={14}
-              height={14}
-              className="ml-1 rounded-full object-cover"
-            />
-          </Link>
-        )}
+        <DeleteThread
+          threadId={JSON.stringify(id)}
+          currentUserId={currentUserId}
+          authorId={author.id}
+          parentId={parentId}
+          isComment={isComment}
+        />
       </div>
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => {
+            return (
+              <Image
+                key={index}
+                src={comment.author.image}
+                alt={`user_${index}`}
+                width={24}
+                height={24}
+                className={`${
+                  index !== 0 && '-ml-5'
+                } rounded-full object-cover`}
+              />
+            );
+          })}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? 'ies' : 'y'}
+            </p>
+          </Link>
+        </div>
+      )}
+      {!isComment && community && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center">
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)} - {community.name} Community
+          </p>
+
+          <Image
+            src={community.image}
+            alt={community.name}
+            width={14}
+            height={14}
+            className="ml-1 rounded-full object-cover"
+          />
+        </Link>
+      )}
     </article>
   );
 };
